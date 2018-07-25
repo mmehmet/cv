3df8a21580b512a5bb319875a6bb9cca3442fde3
MM <mic@computerwiz.info>
2018-07-25 14:11:32 +1000

    update xp

--- experience.php
+++ experience.php
@@ -2,10 +2,16 @@
 		<h3>Experience</h3>
 <?php
 
-$zib = $mehmetech = $cwiz = $staging = $freelance = $intentia = array();
+$lc = $zib = $mehmetech = $cwiz = $staging = $freelance = $intentia = array();
+
+$lc['name'] = "Loyalty Corp";
+$lc['dates'] = "Dec 2016 - ";
+$lc['roles'] = "Senior PHP Developer, DevOps";
+$lc['desc'] = "Primarily using the Laravel framework, working on both the flagship eWallet product as a back-end developer, as well as heading and maintaining the Engagement platform, which is utilised by eWallet as well as directly by Loyalty Corp clients, as a full stack senior developer and operations manager. Both products involved managing AWS resources and assets as well as code reviews and deployments to testing, UAT, and Production resources. The latter also required building a front-end for management of the Gamification projects used by Engagement clients – allowing self-service solutions for building and maintaining Rules, Rewards, Tiers, etc – as well as a Single-Sign-On solution, a Wordpress plugin, and even an iFrame solution for delivering in-app content using JS and Amazon S3.";
+$lc['tech'] = "PHP, Laravel, MySQL, jQuery, Angular, CSS, Bootstrap, git, Bitbucket, Docker, Vagrant, ssh, Slack, Wordpress,  NodeJS, Bower, Gulp, NginX, CORS, JIRA, AWS, REST, SOAP, Composer, oAuth, JWT";
 
 $zib['name'] = "Zib Media / Zibit";
-$zib['dates'] = "Dec 2014 - Present";
+$zib['dates'] = "Dec 2014 - Dec 2016";
 $zib['roles'] = "Senior Web & App Developer, IT Administrator";
 $zib['desc'] = "Full Stack Senior Developer, working across teams in both Melbourne and Adelaide. Directly liaised with stakeholders ranging from clients to external developers to service providers, ensuring smooth delivery which matches expectations as well as limitations of scope. Managed servers and databases and also the company's internal IT systems. Due to noteworthy eye for design and creative flair, consulted on projects in need of a new look or 'jazzing up' - occasionally,  completely rebuilt website from the ground up. Managed and developed both web and native mobile applications, managed dev team and external developer assets, as well as private git repositories. Implemented REST API services, managed and deployed AWS assets, administered linux-based servers, including MySQL-based databases. Developed and implemented <b>bank-level</b> security practices for a FINTECH application with major Australian banking partner. Introduced new collaboration tools, such as Slack and Basecamp. Delivered VPN solution to allow secure access to sensitive data, outside of the office and trained staff how to set up and use it.";
 $zib['tech'] = "HTML5, CSS3, JSON, Java, Objective C, Javascript, jQuery, AWS, git, Wordpress, GIMP, MySQL, ssh/bash, oauth, Slack, Basecamp, Google Apps, PHPStorm, Composer, VPN";
@@ -41,6 +47,7 @@ $intentia['desc'] = "Worked across Operations and Application Engineering (progr
 $intentia['tech'] = "RPG, SQL, Java, AS-400, NT, Solaris";
 
 $xp = array(
+	$lc,
 	$zib,
 	$mehmetech,
 	$cwiz,
