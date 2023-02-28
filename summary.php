d0b3f9fff810776a05a34dc2cf0ca659453da58f
Michael Mehmet <>
2023-02-28 16:40:55 +1100

    update the summary

--- summary.php
+++ summary.php
@@ -1,18 +1,23 @@
 	<div class="section" data-anchor="summary">
 		<h3>Summary</h3>
-		<p>My greatest strength is in being able to analyse a situation and pull together creative solutions by
-            understanding how a wide range of resources work. This means a deeper understanding of the advantages
-            (or disadvantages) of one implementation over another and enough knowledge and experience to be able to roll
-            up the sleeves and problem-solve when necessity dictates.</p>
-		<p>My preferred working environment is <strong>Linux</strong> but my recent experience has been primarily using
-            Macs. In the back-end I have built and worked with custom PHP frameworks, but am just as comfortable using
-            something like <strong>Laravel</strong>. In the front-end, I have built Wordpress themes/plugins and even
-            entire websites with tools like <strong>PHP</strong>, <strong>Typescript</strong>, <strong>React</strong>
-            and <strong>NodeJS</strong></p>
-        <p>I have a degree in <strong>Mathematics</strong> and a masters in <strong>Cognitive Science</strong> and know
-            my way around encryption and deep learning. I built what you are reading, right now. I also have a strong
-            interest in emerging technologies and have explored cryptocurrencies like Bitcoin and their underlying
-            <strong>Blockchain</strong> technology as well as created "AI" algorithms, for fun.</p>
+		<p>My greatest strength is in being able to understand how a wide range of resources work in order to analyse
+            any situation and put together robust, creative solutions. This comes from a depth of knowledge of the
+            advantages/disadvantages of different approaches and the experience and confidence to be able to apply this
+            knowledge and get the job done right.</p>
+		<p>My preferred working environment is Linux Desktop (<a href="https://ubuntu.com/">Ubuntu</a>) but my recent
+            experience has been primarily using Macs. In the back-end I have worked with a variety of
+            <a href="https://www.php.net/releases/8.1/en.php">PHP</a> frameworks, but am most comfortable using
+            <a href="https://github.com/laravel/laravel">Laravel</a>. In the front-end, I have built themes/plugins for
+            platforms like <a href="https://wordpress.org/download/">Wordpress</a> and
+            <a href="https://www.shopify.com/">Shopify</a> and also web apps using tools such as
+            <a href="https://www.typescriptlang.org/">Typescript</a>, <a href="https://reactjs.org/">React</a>
+            and <a href="https://nodejs.org/">Node.js</a></p>
+        <p>I have a degree in <strong>Mathematics</strong> and know my way around topics such as encryption, Bayes'
+            theorem and Fourier analysis, as well as technologies like <strong>Blockchain</strong>. I also have a
+            masters in <strong>Cognitive Science</strong> and a strong interest in related subjects, such as
+            <a href="https://www.ibm.com/topics/deep-learning">Deep learning</a> and
+            <a href="https://openai.com/research/generative-models">Generative models</a>, and have even created my own
+            "AI" algorithms, just for fun.</p>
 		<a href="#education" class="main-nav education col-sm-4">
 			<i class="fa fa-graduation-cap" aria-hidden="true"></i>
 			<p class="h4">education</p>
