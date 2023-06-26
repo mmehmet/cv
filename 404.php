195d9c16b385a561df6de8ecaf467917cb259e22
Michael Mehmet <>
2023-06-26 17:20:01 +1000

    add a 404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 Page Not Found</title>
+    <meta name="description" content="Page Not Found" />
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+</head>
+<body>
+<div style="position: absolute; height: 80%; width: 80%; margin: 5% 0 0 10%; text-align: center; display: table;">
+    <div style="display: table-cell; vertical-align: middle; font-size: 100px;">Nope!</div>
+</div>
+</body>
+</html>
