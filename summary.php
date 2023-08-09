835913890534351988e663d9d530c2556a6b7e2a
Michael Mehmet <>
2023-08-09 14:44:13 +1000

    tweak the summary wording and move paragraphs around

--- summary.php
+++ summary.php
@@ -2,11 +2,11 @@
 
 		<h3>Summary</h3>
 
-		<p>My greatest strength is in being able to analyse any given situation and design creative solutions by understanding how a wide range of resources work &mdash; understanding the advantages/disadvantages of one implementation over another and having the experience to be able to roll up the sleeves and problem-solve when necessity dictates.</p>
+		<p>My greatest strength is in being able to analyse any given situation and design creative solutions by applying a wide range of knowledge &mdash; having the experience to understand the advantage/disadvantage of one implementation over another and be able to roll up the sleeves and problem-solve when necessity dictates.</p>
 
-		<p>In the back-end I have worked with a variety of <a href="https://www.php.net">PHP</a> frameworks (being most familiar with <a href="https://github.com/laravel/laravel">Laravel</a>) but have also developed apps that are entirely based on <a href="https://www.typescriptlang.org/">Typescript</a> and <a href="https://nodejs.org/">Node</a>. In the front-end, I have built themes/plugins for platforms like <a href="https://wordpress.org/download/">Wordpress</a> and <a href="https://www.shopify.com/">Shopify</a> and have also developed apps with <a href="https://reactjs.org/">React</a>.</p>
+        <p>I studied <strong>Mathematics</strong> and <strong>Cognitive Science</strong> at university and I have always been fascinated with the space where these two subjects intersect &mdash; <a href="https://www.ibm.com/topics/deep-learning">Deep learning</a> and <a href="https://openai.com/research">AI</a>. I often spend my spare time building AI projects, not just for the fun of it but also to better understand how it all works.</p>
 
-        <p>I have degrees in both <strong>Mathematics</strong> and <strong>Cognitive Science</strong> and I have always been fascinated with the space where these two subjects intersect &mdash; <a href="https://www.ibm.com/topics/deep-learning">Deep learning</a> and <a href="https://openai.com/research">AI</a>. I often spend my spare time building AI projects, not just for the fun of it but also to better understand how it all works.</p>
+		<p>I have built entire monoliths using <a href="https://www.php.net">PHP</a> frameworks like <a href="https://github.com/laravel/laravel">Laravel</a> and simple microservices made out of <a href="https://www.typescriptlang.org/">Typescript</a> and <a href="https://nodejs.org/">Node</a>. I have also created themes/plugins for platforms like <a href="https://wordpress.org/download/">Wordpress</a> and <a href="https://www.shopify.com/">Shopify</a> and single-page-apps using <a href="https://reactjs.org/">React</a>.</p>
 
 		<a href="#education" class="main-nav education col-sm-4">
 			<i class="fa fa-graduation-cap" aria-hidden="true"></i>
